98b4b1fbd071930771652a97f7ef9f26dee83b49
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2021-10-22 21:06:46 -0500

    creation of the project structure

---
+++ src/layouts/defatult.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        <NUXT></NUXT>
+    </section>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        <div>App Nuxt 3</div>
+        <a-date-picker :locale="locale" />
+    </section>
+</template>
+<script>
+  import locale from 'ant-design-vue/es/date-picker/locale/es_ES';
+  export default {
+    data() {
+      return {
+        locale,
+      };
+    },
+  };
+</script>
